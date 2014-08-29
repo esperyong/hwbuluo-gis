792b81bfa1081a75bf97d53ff6189797bc28b329
esperyong <esperyong@gmail.com>
2014-08-29 15:59:40 +0800

    增加了一个方法用来序列化单独的一个Point对象

--- hwbuluocontrib/gis/geojson/__init__.py
+++ hwbuluocontrib/gis/geojson/__init__.py
@@ -2,14 +2,25 @@
 from __future__ import unicode_literals
 import json
 
-def simple_pointset_togeojson(queryset,properties):
+def simple_point_togeojson(point_instance,properties):
+    """
+    将一个django point转换为geojson格式
+    对象必须包含有名为point的字段,并且有x,y,z属性
+    """
+    result = {"type": "Feature",
+              "geometry":{"type":"Point",
+                          "coordinates": [getattr(point_instance.point,i) for i in ('x','y','z') if getattr(point_instance.point,i)]},
+              "properties":{ field_name:getattr(point_instance,field_name,'') for field_name in properties}
+              }
+    return json.dumps(result)
+
+def simple_pointset_togeojson(points,properties):
     """
     将一个django point集合转换为geojson格式
     对象必须包含有名为point的字段,并且有x,y,z属性
-    
     """
     result = {"type": "FeatureCollection","features":[]}
-    if queryset:
+    if points:
         result['features'] = [ {'type':'Feature',
                       "geometry": {
                           "type": "Point",
@@ -18,6 +29,5 @@ def simple_pointset_togeojson(queryset,properties):
                        },
                       "properties":{ field_name:getattr(instance,field_name,'') 
                                                     for field_name in properties}} 
-                                                          for instance in queryset]
+                                                          for instance in points]
     return json.dumps(result)
-
