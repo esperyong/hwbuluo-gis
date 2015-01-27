258454be99511fe287cc8c691351eefd07eb98dd
esperyong <esperyong@gmail.com>
2015-01-27 08:42:48 +0800

    重构代码，将serializer和render操作分开

--- hwbuluocontrib/gis/geojson/__init__.py
+++ hwbuluocontrib/gis/geojson/__init__.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 import json
 
-def simple_point_togeojson(point_instance,properties):
+def simple_point_to_dict(point_instance,properties):
     """
-    将一个django point转换为geojson格式
+    将一个django point转换为python dict格式
     对象必须包含有名为point的字段,并且有x,y,z属性
     """
     result = {"type": "Feature",
@@ -12,13 +12,18 @@ def simple_point_togeojson(point_instance,properties):
                           "coordinates": [getattr(point_instance.point,i) for i in ('x','y','z') if getattr(point_instance.point,i)]},
               "properties":{ field_name:getattr(point_instance,field_name,'') for field_name in properties}
               }
-    return json.dumps(result)
+    return result
 
-def simple_pointset_togeojson(points,properties):
+
+def simple_point_togeojson(point_instance,properties):
     """
-    将一个django point集合转换为geojson格式
+    将一个django point转换为geojson格式
     对象必须包含有名为point的字段,并且有x,y,z属性
     """
+    return json.dumps(simple_point_to_dict(point_instance, properties))
+
+
+def simple_pointset_to_dict(points,properties):
     result = {"type": "FeatureCollection","features":[]}
     if points:
         result['features'] = [ {'type':'Feature',
@@ -30,4 +35,11 @@ def simple_pointset_togeojson(points,properties):
                       "properties":{ field_name:getattr(instance,field_name,'') 
                                                     for field_name in properties}} 
                                                           for instance in points]
-    return json.dumps(result)
+    return result
+
+def simple_pointset_togeojson(points,properties):
+    """
+    将一个django point集合转换为geojson格式
+    对象必须包含有名为point的字段,并且有x,y,z属性
+    """
+    return json.dumps( simple_pointset_to_dict(points, properties) )
